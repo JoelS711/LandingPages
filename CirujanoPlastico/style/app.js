4dd7d51d90bf9bb5bfc72660e21be3281894c2fc
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-03-04 13:19:13 -0500

    Se crea carrusel de imagenes de la galeria y se ajusta el responsive

---
+++ CirujanoPlastico/style/app.js
@@ -0,0 +1,16 @@
+
+
+/*const swiper = new swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+        depth: 500,
+        modifer:1,
+        slidesShadows: true,
+        rotate:0,
+        stretch:0
+    }
+})*/
